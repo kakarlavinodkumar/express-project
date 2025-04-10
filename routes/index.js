1f38e46a262d6c23b108bb19ee275ee199f47549
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-09 21:48:20 -0700

    Add JSDoc comments for home page route in the index router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * Express router module for handling the home page route.
+ * 
+ * @module routes/index
+ */
+
+ /**
+  * GET home page.
+  * Renders the index view with a title.
+  * 
+  * @name GET /
+  * @function
+  * @memberof module:routes/index
+  * @param {Object} req - Express request object.
+  * @param {Object} res - Express response object.
+  * @param {Function} next - Express next middleware function.
+  */
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
